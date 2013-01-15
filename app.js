049fbcb29dc04d1213159296a6e8be6794e174d1
Duncan Kolba <duncankolba@gmail.com>
2013-01-15 20:19:38 +0000

    defined redis process.env variables

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ var http = require("http")
   , flatiron = require("flatiron")
   , director = require("director")
   , redis = require("redis")
-  , redisClient = redis.createClient()
-  , app = flatiron.app
+  , redisClient = redis.createClient(process.env.redisport, process.env.host)
+  , app = flatiron.app;
+
+  redisClient.auth(process.env.redissecret);
