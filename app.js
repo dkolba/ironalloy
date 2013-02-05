bf7c339a85adb57af741a38846d4801f11a3e823
Duncan Kolba <duncankolba@gmail.com>
2013-02-05 20:57:02 +0000

    Added Plates as templating engine

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 "use strict"
 
 var http = require("http")
+  , fs = require("fs")
   , flatiron = require("flatiron")
   , director = require("director")
   , union = require("union")
+  , plates = require("plates")
   , redis = require("redis")
   , redisClient = redis.createClient(process.env.redisport, process.env.host)
   , app = flatiron.app;
@@ -12,23 +14,31 @@ redisClient.auth(process.env.redissecret);
 
 app.use(flatiron.plugins.http);
 
-function helloWorld() {
-  this.res.writeHead(200, { 'Content-Type': 'text/plain' })
-  this.res.end('hello world');
-};
+// Read template from file, render via plates and send response
+function gettemplate () {
+  var that = this 
+  fs.readFile("templates/template.html", "utf8", function (err, data) {
+    if(err) throw err;
+    var content = { "test": "New Value" }
+      , output = plates.bind(data, content); 
+    that.res.writeHead(200, { 'Content-Type': 'text/html' });
+    that.res.end(output);
+    console.log(output);
+  });
+}
 
-// define routing table
+// Define routing table
 var routes = {
   '/hello' : {
     '/:test': {
       get: function (test) {redisClient.set("urls", test);
              console.log(test)}
     },
-    get: helloWorld
+    get: gettemplate
   }
 };
 
-// inject routing table
+// Inject routing table
 app.router.mount(routes);
 
 app.start(8080);
