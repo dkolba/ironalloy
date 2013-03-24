28a530902294ad57214525fe9d51a81bc585a20e
Duncan Kolba <duncankolba@gmail.com>
2013-03-24 10:21:06 +0000

    Foundation for tests (via mocha/chai)

---
+++ test/testtemplate.js
@@ -0,0 +1,11 @@
+var  mocha = require('mocha');
+var   chai = require('chai');
+var should = chai.should();
+var    app = require(__dirname + "/../app");
+
+describe('blablabla', function() {
+  it('should be bla bla bla', function() {
+    var something = blablabla.bla;
+  something.should.equal('xyz');
+  });
+});
